a4b7c10070e117908643e6a4ad3c550567b1b9a6
kakeru-516 <ue.0516@gmail.com>
2022-09-10 22:17:34 +0900

    add .gitignore and run.sh

--- shell_script/template/run.sh
+++ shell_script/template/run.sh
@@ -1,4 +1,5 @@
 docker run --privileged --name container_name\
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix: \
+    -v `pwd`/src:/root/catkin_ws/src \
     -itd ubuntu20.04:ros-noetic /bin/bash
